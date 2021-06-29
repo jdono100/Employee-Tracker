64aadb51fa926fb13f4bd9825bf53590e182f438
Jared Donovan <jdono100@gmail.com>
2021-06-29 03:40:44 -0500

    department adding/removing finished

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const init = async function() {
             message: chalk.yellow('What would you like to do?'),
             choices: [
                 new inquirer.Separator(),
-                chalk.blue('View All Departments'),
-                chalk.blue('Add A Department'),
-                chalk.blue('Delete A Department'),
+                chalk.cyan('View All Departments'),
+                chalk.cyan('Add A Department'),
+                chalk.cyan('Delete A Department'),
                 new inquirer.Separator(),
                 chalk.green('View All Roles'),
                 chalk.green('Add A Role'),
@@ -44,13 +44,13 @@ const init = async function() {
         }
     ]).then(function(res) {
         switch (res.options) {
-            case chalk.blue('View All Departments'):
+            case chalk.cyan('View All Departments'):
                 viewDepartments();
                 break;
-            case chalk.blue('Add A Department'):
+            case chalk.cyan('Add A Department'):
                 addDepartment();
                 break;
-            case chalk.blue('Delete A Department'):
+            case chalk.cyan('Delete A Department'):
                 deleteDepartment();
                 break;
             case chalk.green('View All Roles'):
@@ -76,6 +76,115 @@ const init = async function() {
                 break;
         }
     })
+}
+
+const viewDepartments = function() {
+    con.query('SELECT * FROM departments',
+    (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log(chalk.dim.red('No departments added yet'));
+        } else {
+            console.table('Departments:', res);
+        }
+        init();
+    })
+}
+
+const addDepartment = function() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'deptName',
+            message: 'Please enter a name for the department',
+            validate: (input) => {
+                if (!input) {
+                    console.log(chalk.bold.red('A name is required for new departments'));
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then(function(res) {
+        let departmentName = res.deptName
+        con.query('INSERT INTO departments SET ?',
+        {
+          name: departmentName
+        });
+        console.log(chalk.cyan(`Department '${departmentName.toLowerCase()}' has been added`));
+        init();
+    })
+}
+
+const deleteDepartment = function() {
+    con.query('SELECT departments.id, departments.name FROM departments',
+    async (err, res) => {
+      if (err) throw err;
+      if (await res.length === 0) {
+        console.log(chalk.dim.red('No departments to delete'));
+        init();
+      } else {
+      let deptNamesArr = [];
+      res.forEach((departments) => {
+          deptNamesArr.push(departments.name);
+      });
+      await inquirer.prompt([
+          {
+              type: 'list',
+              name: 'deletedDept',
+              message: 'Please choose a department to remove',
+              choices: deptNamesArr
+          }
+      ]).then(async (response) => {
+          let deptId;
+          await res.forEach((departments) => {
+              if (response.deletedDept === departments.name) {
+                  deptId = departments.id;
+          }
+        })
+        con.query('DELETE FROM departments WHERE departments.id = ?',
+        [deptId], (err) => {
+          if (err) throw err;
+          console.log(chalk.cyan('Department successfully deleted'));
+          init();
+        })
+      })
+    }
+  })
+}
+
+const viewRoles = function() {
+  con.query('SELECT * FROM roles',
+    (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log(chalk.dim.red('No roles added yet'));
+        } else {
+            console.table('Roles:', res);
+        }
+      init();
+  })
+}
+
+const addRole = function() {
+
+}
+
+const deleteRole = function() {
+
+}
+
+const viewEmployees = function() {
+
+}
+
+const addEmployee = function() {
+
+}
+
+const deleteEmployee = function() {
+  
 }
 
 const exitApp = function() {
